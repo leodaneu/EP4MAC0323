65b798b36e8ff3b123059cbb61c6b003b790175e
BlackJuice <lucas.sj.hong@gmail.com>
2015-07-09 16:42:21 -0300

    impressão da imagem usando .vll
    ignora o node

--- PlotFilter.java
+++ PlotFilter.java
@@ -14,10 +14,18 @@ public class PlotFilter {
     public static void main(String[] args) {
 
         // read in bounding box and rescale
+        /*
         double x0 = StdIn.readDouble();
         double y0 = StdIn.readDouble();
         double x1 = StdIn.readDouble();
         double y1 = StdIn.readDouble();
+        */
+        double x0 = 46.74;
+        double y0 = 23.57;
+
+        double x1 = 46.72;
+        double y1 = 23.55;
+
         StdDraw.setXscale(x0, x1);
         StdDraw.setYscale(y0, y1);
 
@@ -26,8 +34,15 @@ public class PlotFilter {
 
         // plot points, one at a time
         while (!StdIn.isEmpty()) {
-            double x = StdIn.readDouble();
+            double ignore = StdIn.readDouble();
             double y = StdIn.readDouble();
+            y = y * (-1);
+            //x = ( x * (-1) ) - 23;
+            //x = ( x + 23 ) * (- 1);
+            double x = StdIn.readDouble();
+            x = x * (-1);
+            //y = ( y + 46 ) * (- 1);
+            StdOut.println(x + " " + y);
             StdDraw.point(x, y);
         }
 
